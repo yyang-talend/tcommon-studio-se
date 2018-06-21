7cafee41c972eb4d95ad7b4cb0d52073740a35f5
apoltavtsev <apoltavtsev@gmail.com>
2018-06-21 18:33:48 +0300

    Fix for issue with incorrect dependency name

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/AbstractMavenProcessorPom.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/creator/AbstractMavenProcessorPom.java
@@ -284,6 +284,11 @@ public abstract class AbstractMavenProcessorPom extends CreateMavenBundleTemplat
                     property = jobInfo.getProcessItem().getProperty();
                     groupId = PomIdsHelper.getJobGroupId(property);
                     artifactId = PomIdsHelper.getJobArtifactId(jobInfo);
+                    // add artifact suffix "-bundle" for child job referenced by cTalendJob in OSGI build mode
+                    Object buildType = (getJobProcessor().getArguments() != null)?  getJobProcessor().getArguments().get("BUILD_TYPE") : null;
+                    if(buildType != null && buildType.equals("ROUTE") && ERepositoryObjectType.getType(property).equals(ERepositoryObjectType.PROCESS)) {
+                        artifactId+="-bundle";
+                    }
                     version = PomIdsHelper.getJobVersion(property);
                     // try to get the pom version of children job and load from the pom file.
                     String childPomFileName = PomUtil.getPomFileName(jobInfo.getJobName(), jobInfo.getJobVersion());
